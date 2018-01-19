9603369c6901641f224fa395fc8f0b3c4fd1e633
Kyle Szeto <kyl3sz3t0@gmail.com>
2018-01-18 16:54:00 -0800

    installing dependencies

--- picture-search/src/search-bar/search-bar.js
+++ picture-search/src/search-bar/search-bar.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
-export default function SearchBar() {
-  return (
-    <div>
-        <input type="text" placeholder="Search for image"/>
-        <button>Search</button>
-    </div>
-  );
+export default class SearchBar extends React.Component {
+  onSubmit(e) {
+    e.preventDefault();
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={(e)=> this.onSubmit(e)}>
+          <input type="text" placeholder="Search for image"
+                ref={input => this.textInput = input}/>
+          <button type='submit'>Search</button>
+        </form>
+      </div>
+    );
+  }
 }
