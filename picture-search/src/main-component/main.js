dad9d626476e6ac52bb69ed9cd2b2d0db5741a3c
Kyle Szeto <kyl3sz3t0@gmail.com>
2018-01-19 14:46:30 -0800

    finish main css

--- picture-search/src/main-component/main.js
+++ picture-search/src/main-component/main.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import SearchBar from '../search-bar/search-bar.js';
 import ShowPictures from '../show-pictures/show-pictures.js';
-import './main.css';
 
 export default class Main extends React.Component {
   render() {
     return (
-      <div>
+      <main>
         <h1>Image Searcher</h1>
         <SearchBar />
         <ShowPictures />
-      </div>
+      </main>
     );
   }
 }
