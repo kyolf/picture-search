3c45fbe1624bb2dc6944cf6d83d2f4578b5451c1
Kyle Szeto <kyl3sz3t0@gmail.com>
2018-01-19 00:27:25 -0800

    got the pictures to be showing

--- picture-search/src/show-pictures/show-pictures.js
+++ picture-search/src/show-pictures/show-pictures.js
@@ -5,16 +5,25 @@ import { link } from 'fs';
 export class ShowPictures extends React.Component{
   renderPictures() {
     console.log(this.props.imageList);
-    let imgList = this.props.imageList;
-    let limitTenImgs = imgList.filter((el,index) => index < 10);
+    let imgList = this.props.imageList.images;
+    console.log(imgList);
+    let limitTenImgs = [];
+    if(typeof imgList !== "undefined" && imgList.length !== 0){
+      for(let i = 0; i < 10; i++) {
+        console.log('hi', imgList[i]);
+        limitTenImgs.push(imgList[i]);
+      }
+    }
     console.log(limitTenImgs);
-    return limitTenImgs.map((el, i) => {
-      return <aside key={i}> 
-               <img src={`${imgList[i].display_sizes[0].uri}`} 
-                   alt={`${imgList[i].title}`} />
-               <p>{imgList[i].title}</p>
-             </aside>;
-    })
+    if(limitTenImgs.length !== 0) {
+      return limitTenImgs.map((el, i) => {
+        return <aside key={i}> 
+                <img src={`${imgList[i].display_sizes[0].uri}`} 
+                    alt={`${imgList[i].title}`} />
+                <p>{imgList[i].title}</p>
+              </aside>;
+      })
+    }
   }
   
   render() {
