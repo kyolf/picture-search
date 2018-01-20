13749e5b69f478ade3d55db497a874a6314af930
Kyle Szeto <kyl3sz3t0@gmail.com>
2018-01-19 20:18:00 -0800

    got the nonletter spellchecker to work

--- picture-search/src/search-bar/search-bar.js
+++ picture-search/src/search-bar/search-bar.js
@@ -3,10 +3,45 @@ import {connect} from 'react-redux';
 
 import {searchForImages} from './search-bar-action.js';
 
+import wordList from 'word-list-json';
+
 export class SearchBar extends React.Component {
   onSubmit(e) {
     e.preventDefault();
-    this.props.dispatch(searchForImages(this.textInput.value))
+    let word = this.textInput.value;
+    this.textInput.value = '';
+    
+    if(word === '') {
+      alert('Empty Input');
+    }
+
+    if(wordList.includes(word.toLowerCase())) {
+      this.props.dispatch(searchForImages(word));
+    }
+    else {
+      this.checkForNonLetter(word);
+    }
+  }
+
+  checkForNonLetter(word) {
+    let newWord = '';
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    for(let i = 0; i < word.length; i++) {
+      if(alphabet.includes(word.charAt(i))) {
+        newWord += word.charAt(i);
+      }
+    }
+    console.log(newWord);
+    if(wordList.includes(newWord)) {
+      this.props.dispatch(searchForImages(newWord));
+    }
+    else {
+      //this.checkForVowels(newWord);
+    }
+  }
+
+  checkForVowels(word) {
+    
   }
 
   render() {
